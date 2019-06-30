19c71c9f07aa21cb8caf316e73c7b60a9c007630
Elvis <elvis@MacBook-Pro-de-Elvis.local>
2019-06-30 12:12:16 -0500

    ultimo cambio

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,5 @@
 insert into supplier values (2090990290, 'IBM', 'ibm@gmail.com', 989902900);
 insert into supplier values (2090990290, 'Claro', 'claro@gmail.com', 989902870);
+
+insert into product values (001,'laptop', 20, 2200.00);
+insert into product values (002,'mouse', 29, 50.50);
